04ee2d6a1a757c9c106baf8730a4345b8f4ce988
shii-causal <suzuhime.27@icloud.com>
2024-01-09 23:53:10 +0900

    ブログ投稿一覧の表示

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Blog-posts</title>
+    <link rel="stylesheet" href="{{ secure_asset('/assets/css/index.css') }}">
+  </head>
+
+  <body>
+    <h1 class="blog">Blog Name</h1>
+    
+    <div class="posts">
+        
+        @foreach ($posts as $post)
+        
+            <div class="post">
+            
+                <h2 class="title">{{ $post->title }}</h2>
+                <p class="body">{{ $post->body }}</p>
+    
+            </div>
+            
+        @endforeach    
+            
+    </div>
+  </body>
+  
+</html>
